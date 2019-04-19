6bdac12bf79da583ca5718a5599ce8a7056c4cf0
apizarras <angelalee.p@gmail.com>
2019-04-19 16:18:08 -0500

    style reset button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -205,3 +205,28 @@ database.ref("/tracks").on("child_added", function(snapshot) {
 });
 //the line below ends the on document ready function
 });
+
+//get lyrics
+// created function to get song by title making an ajax call to the indicated database
+function getSongs(songTitle){
+    // created url variable to call song from returned array from database also making an ajax call. 
+    const artist= "Coldplay"
+    const title= "Adventure of a Lifetime"
+    const url = "https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime";
+    $.ajax({
+         url: url, 
+         method:"GET"
+     }).then(function(response){
+         console.log(response);
+        //Used filter method to compare selected song title queried from the URL with returned value in the response
+         const thisSong = response.filter(function(song){return song.title === songTitle})[0];
+// ajax call to get lyrics, embed song title in html using element id, embed song in html using element id.
+         $.ajax({
+             url: `${url}/${thisSong.id}/lyrics`, 
+             method:"GET"
+         }).then(function(lyrics){
+             $("#lyricHeader").text(songTitle)
+             $("#lyricZone").text(lyrics);
+         });
+     });
+ };
